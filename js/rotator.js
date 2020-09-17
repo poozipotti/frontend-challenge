2e2b9b9a2e09ddde639f3237ad898b7844623187
George <georgebeckert@gmail.com>
2020-09-17 02:12:12 -0400

    finished rotator

---
+++ js/rotator.js
@@ -0,0 +1,18 @@
+//heavily inspired from this article https://medium.com/@marcusmichaels/how-to-build-a-carousel-from-scratch-in-vanilla-js-9a096d3b98c9
+
+let rotatorItems = []
+let selectorClass = 'rotatorContainer'
+let count = 0
+window.onload = function loadFrame() {
+  rotatorItems = document.getElementsByClassName(selectorClass)
+  setInterval(updateRotator, 500)
+}
+
+function updateRotator() {
+  console.log('updated');
+  rotatorItems[count].classList.add('unactive')
+  rotatorItems[count].classList.remove('active')
+  count = (count + 1) % rotatorItems.length
+  rotatorItems[count].classList.remove('unactive')
+  rotatorItems[count].classList.add('active')
+}
